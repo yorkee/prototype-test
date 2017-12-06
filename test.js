5504025e3110e3074ea121a30a9a85e6e06fdd55
Nancy Yeung <nancyyeung@booky.local>
2017-12-06 17:04:39 -0500

    break out clock into its own module

--- test.js
+++ test.js
@@ -1,39 +1,11 @@
 'use strict';
 
 var assert = require('assert');
+var Clock = require('./clock');
 
 describe('test using prototype', function(){
-	let at, Clock;
+	let at;
 	before(function(){
-		//TODO:  break it out into clock.js
-		Clock = function(hr, min){
-			this.hr = hr || 0;
-			this.min = min || 0;
-		};
-
-		Clock.prototype.toString = function(){
-			function parseTimeDigit(digit){
-				if (digit){
-					if(digit < 10){
-						return "0" + digit;
-					} else {
-						return digit;
-					}
-				} else {
-					return "00";
-				}
-			}
-			return parseTimeDigit(this.hr) + ":"+ parseTimeDigit(this.min);
-		}
-
-		Clock.prototype.plus = function(plusMin) {
-			this.min = this.min + plusMin;
-			let hrToAdd = Math.floor(this.min / 60);
-			this.min = this.min % 60;
-			this.hr += hrToAdd;
-			return this;
-		}
-
 		at = function(hr, min){
 			return new Clock(hr, min);
 		}
